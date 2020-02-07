ac7aa9b08c1461c69dcea2eb2e4d69bacd83ed51
Souza <debora.souza@wunderman.com>
2020-02-07 18:53:16 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import data from './data/cities.json'
 
 function App() {
+
+  const [searchCity, setSearchCity] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const modal = true
+  const cidades = []
+
+  const handleChange = e => {
+    setSearchCity(e.target.value)
+  }
+
+  const getCities = () => {
+    
+    data.estados.map((estados) => (
+      estados.cidades.map(x => (
+        cidades.push(x)
+      ))
+    ))
+    
+  }
+
+  useEffect(() => {
+    getCities()
+    const results = cidades.filter(city => 
+      city.toLowerCase().includes(searchCity.toLocaleLowerCase())
+    )
+    setSearchResults(results)
+  }, [searchCity])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text"
+        placeholder="Informe a sua cidade"   
+        value={searchCity}    
+        onChange={handleChange}
+      />
+      {modal && searchCity !== '' &&
+      <ul>
+        {searchResults.map((city, i) => (
+          <li key={i}>{city}</li>
+        ))}      
+      </ul>
+      }
     </div>
   );
 }
